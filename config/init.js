0e796dcc2a3eb0fb3362f609dcb48305d8487a43
zhangzhuangzhuang <zhangzhuangzhuang@qq.com>
2020-11-11 00:02:10 +0800

    '创建mysql数据库'

---
+++ config/init.js
@@ -0,0 +1,12 @@
+const config = {
+  port: 3000,
+  //数据库设置
+  mysql: {
+    database: 'test',
+    username: 'root',
+    password: '123456',
+    port: '3306',
+    host: 'localhost'
+  }
+}
+module.exports = config;
