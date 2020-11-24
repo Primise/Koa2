38d2983a28289c129aaeac4cf4b202acee07e438
zhangzhuangzhuang <zhangzhuangzhuang@qq.com>
2020-11-24 22:28:31 +0800

    设计用户表

--- controller/users.js
+++ controller/users.js
@@ -1,4 +1,12 @@
-const user = require('../model/article');
+/*
+ * @Author: your name
+ * @Date: 2020-11-14 14:52:20
+ * @LastEditTime: 2020-11-24 22:19:01
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Koa2\controller\users.js
+ */
+const User = require('../model/article');
 
 
 /**
@@ -8,6 +16,7 @@ const user = require('../model/article');
 exports.addUser = async ctx=>{
   let {name,pass} = ctx.request.body;
   console.log(name,pass);
+
   // user.addUser(name,pass).then(res=>{
   //   console.log('注册成功'+res);
   //   ctx.body={
---
+++ model/user.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-24 22:19:11
+ * @LastEditTime: 2020-11-24 22:28:29
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Koa2\model\user.js
+ */
+
+const Sequelize = require('sequelize');
+const sequelize = require('../sequelize');
+
+
+
+
+//创建Model数据库模型
+let User = sequelize.define('user',{
+  
+}) 
