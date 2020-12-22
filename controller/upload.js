76d004407bb693182dd9e1a6f69f045582492689
zhangzhuangzhuang <zhangzhuangzhuang@qq.com>
2020-12-22 23:01:03 +0800

    上传图片功能

--- controller/upload.js
+++ controller/upload.js
@@ -2,22 +2,71 @@
  * @Description: 上传图片
  * @Author: primsie7
  * @Date: 2020-12-03 11:09:47
- * @LastEditTime: 2020-12-22 10:57:53
+ * @LastEditTime: 2020-12-22 22:52:09
  */
 const fs = require("fs");
 const path = require("path");
+const { timeFormat } = require('../utils/utils')
+// const timeFormat = require("../utils/utils")
+// 递归创建目录 同步方法
+function checkDirExist(dirname) {
+  if (fs.existsSync(dirname)) {
+    return true;
+  } else {
+    if (checkDirExist(path.dirname(dirname))) {
+      fs.mkdirSync(dirname);
+      return true;
+    }
+  }
+}
+// 生成新的文件名称
+function getUploadFileExt(name) {
+  let ext = name.split('.');
+  let first = name.replace(ext[ext.length - 1], '');
+  let last = timeFormat(new Date(), 'yyyy-mm-dd-HH-mm-ss')
+  return `${first}${last}.${ext[ext.length - 1]}`
+}
 
 class UploadServer {
   // 上传图片到七牛
   static async upload(ctx) {
     try {
       //上传单个文件
-      console.log(ctx.request)
+      // console.log(ctx.request.files)
+      const { bucket } = ctx.request.body;
       const file = ctx.request.files.file;//获取上传文件
-      
+      // 创建可读流
+      const reader = fs.createReadStream(file.path);
+
+      // 生成文件夹
+      let dir = path.join(__dirname, `../public/upload/${bucket}`);
+      checkDirExist(dir)
+
+      // 生成图片文件名字
+      let newName = getUploadFileExt(file.name);
+      console.info('newName', newName);
+
+      // 文件目录
+      let filePath = `${dir}/${newName}`
+
+      // 创建可写流
+      const upStream = fs.createWriteStream(filePath);
+      // console.info('upStream', upStream)
+
+      // 可读流通过管道写入可写流
+      reader.pipe(upStream);
+      let urlstr = `${ctx.origin}/upload/${bucket}/${newName}`
+
+      const data = {
+        url: urlstr,
+        createTime: +new Date()
+      }
+      ctx.success(data, null, "操作成功")
+
+
     } catch (error) {
       console.log(error)
-        ctx.fail(500,"上传失败")
+      ctx.fail(500, "上传失败")
     }
   }
 }
--- utils/utils.js
+++ utils/utils.js
@@ -3,8 +3,33 @@
  * @version: 
  * @Author: primsie7
  * @Date: 2020-11-19 11:16:16
- * @LastEditTime: 2020-11-24 14:14:08
+ * @LastEditTime: 2020-12-22 22:41:44
  */
+ function timeFormat(date, type) {
+  var time = new Date(date);
 
+  var year = time.getFullYear()
+  var month = time.getMonth() < 9 ? "0" + (time.getMonth() + 1) : time.getMonth() + 1
+  var day = time.getDate() < 10 ? "0" + time.getDate() : time.getDate()
+
+  var hour = time.getHours() < 10 ? "0" + time.getHours() : time.getHours()
+  var minute = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes()
+  var second = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds()
+  if (type === "yyyy-mm-dd") {
+      return [year, month, day].join("-")
+  } else if (type === "mm-dd") {
+      return [month, day].join("-")
+  } else if (type === "MM-DD") {
+      return `${month}月${day}日`
+  } else if (type === "yyyy-mm-dd-HH-mm-ss") {
+      return [year, month, day, hour, minute, second].join("-")
+  } else {
+      return [year, month, day].join("-") + " " + [hour, minute, second].join(":")
+  }
+}
+
+module.exports={
+  timeFormat
+}
 
 
